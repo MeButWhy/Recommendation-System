d62f3e5f2ddb48c0bd7997ca0b190a431b27f180
MeButWhy <johanboers94@gmail.com>
2016-02-26 19:13:56 +0100

    .

---
+++ RecommendationSystem/RecommendationSystem/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> userPreferences = new Dictionary<int, int>();
+
+            string line = "";
+            using (StreamReader writer = new StreamReader(@"C:\Users\Johan\Desktop\INFDTA01-1\userItem.data"))
+            {
+                while ((line = writer.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadLine();
+
+            //{
+            //    foreach (var line in File.ReadLines(filename))
+            //    {
+            //        foreach (var num in line.Split(','))
+            //        {
+            //            writer.Write(num + " ");
+            //        }
+            //    }
+            //}
+
+            
+
+
+
+
+            //System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Johan\Desktop\INFDTA01-1\userItem.data");
+
+            
+
+            //foreach (var line in file.ReadLines()
+            //{
+            //    foreach (var num in line.Split(','))
+            //    {
+            //        userPreferences.Add(int.Parse(num[0]), int.Parse(num[1]));
+            //    }
+            //}
+
+            //foreach (DataRow row in file.Tables[0].Rows)
+            //{
+            //    string[] columns = line.Split(',');
+
+            //}               
+
+            // Read the file as one string.
+            //string text = System.IO.File.ReadAllText(@"C:\Users\Johan\Desktop\INFDTA01-1\userItem.data");
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+        }
+    }
+}
---
+++ RecommendationSystem/RecommendationSystem/UserPreference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecommendationSystem
+{
+    class UserPreference
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+    }
+
+    class CollInit
+    {
+        Dictionary<int, UserPreference> userPreferences = new Dictionary<int, UserPreference>()
+        {
+            {1, new UserPreference {FirstName="Johan", LastName="Boers", ID=211 } },
+            //{, new UserPreference { , ,  } }
+        };
+    }
+}
